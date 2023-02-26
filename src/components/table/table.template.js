01e2fbe1e094ad88ca55de3cad5cd9669664262d
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2023-02-26 17:26:41 +0300

    add resize column

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -12,14 +12,21 @@ function toCell() {
 
 function toColumn(col) {
   return `
-    <div class = "column">${col}</div>`;
+    <div class = "column" data-type = "resizable"> ${col}
+    <div class = "col-resize" data-resize = "col"></div>
+    </div>`;
 }
 
-function createRow(index, content) {
-  return `
 
+function createRow(index, content) {
+  const resize = index
+    ? ` <div class = "row-resize" data-resize = "row" ></div>`
+    : "";
+  return `
     <div class = "row"> 
-     <div class = "row-info">${index ? index : " "} </div>
+     <div class = "row-info">${index ? index : " "}
+     ${resize}
+     </div>
     <div class = "row-data">${content}</div>
     </div>
    
@@ -34,13 +41,12 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
   const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
-  console.log(cols);
 
   rows.push(createRow(null, cols));
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount).fill("").map(toCell).join("");
 
-    rows.push(createRow(i+1, cells));
+    rows.push(createRow(i + 1, cells));
   }
 
   return rows.join("");
--- src/core/dom.js
+++ src/core/dom.js
@@ -41,6 +41,12 @@ class Dom {
 
     return this;
   }
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+  getCoords() {
+    return this.$el.getBoundingClientRect();
+  }
 }
 
 // event.target
